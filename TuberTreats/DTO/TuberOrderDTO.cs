138ae7d212531551f3c4f0a72b21f980fa280de4
Chris <christopherpack17@gmail.com>
2025-09-01 18:16:42 -0500

    small clean up

--- TuberTreats/DTO/TuberOrderDTO.cs
+++ TuberTreats/DTO/TuberOrderDTO.cs
@@ -5,12 +5,3 @@ public class TuberOrderDTO
   public int CustomerId { get; set; }
   public List<int> ToppingId { get; set; } = [];
 }
-
-// DTO for creating a new order
-public class CreateOrderRequest
-{
-  public int WheelId { get; set; }
-  public int TechnologyId { get; set; }
-  public int PaintId { get; set; }
-  public int InteriorId { get; set; }
-}
